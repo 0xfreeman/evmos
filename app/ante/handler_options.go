8a7b9ec7b9cc6280fbc60197b4e155f1e688b554
Tingwei Liu <liutingwei3996@gmail.com>
2023-11-14 23:53:41 +0800

    remove distribution keeper

--- app/ante/handler_options.go
+++ app/ante/handler_options.go
@@ -85,9 +85,6 @@ func (options HandlerOptions) Validate() error {
 	if options.SignModeHandler == nil {
 		return errorsmod.Wrap(errortypes.ErrLogic, "sign mode handler is required for AnteHandler")
 	}
-	if options.DistributionKeeper == nil {
-		return errorsmod.Wrap(errortypes.ErrLogic, "distribution keeper is required for AnteHandler")
-	}
 	if options.TxFeeChecker == nil {
 		return errorsmod.Wrap(errortypes.ErrLogic, "tx fee checker is required for AnteHandler")
 	}
